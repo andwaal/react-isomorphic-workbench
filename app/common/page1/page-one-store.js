9fd87c5be617c0b00c6cbebfc1489ed576708c76
andwaal <a_waal@hotmail.com>
2015-02-15 20:19:01 +0100

    More experiments

---
+++ app/common/page1/page-one-store.js
@@ -0,0 +1,37 @@
+(function() {
+    "use strict";
+
+    var Reflux = require('reflux');
+    var Dispatcher = require('../actions');
+
+
+    var Page1Store = Reflux.createStore({
+        listenables: Dispatcher,
+        init:function(){
+            this.clickedBtn = 'none';
+        },
+        onBtnClicked: function(name){
+            this.clickedBtn = name;
+            this.onHydrate();
+        },
+        onStoreHydrate : function(initialState){
+            if(initialState.pageOne){
+                this.header = initialState.pageOne.header;
+                this.description = initialState.pageOne.description;
+                this.clickedBtn = initialState.pageOne.clickedBtn;
+            }
+        },
+        onPage1RequestState : function(){
+            this.trigger(
+                {
+                header : this.header,
+                description : this.description,
+                clickedBtn : this.clickedBtn
+            });
+        }
+    });
+    
+
+    module.exports = Page1Store;    
+
+}).call(this);
